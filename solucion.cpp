d49a8292786ecb7b294801944981861498d325f8
AlejandroLarra <90375233+AlejandroLarra@users.noreply.github.com>
2022-04-05 11:00:54 -0400

    Add files via upload

---
+++ solucion.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hola me llamo Eddie Alejandro Larrazabal Frias, tengo 25 años y estudio tecnologias de la informacion y seguridad" << endl;
+	cout << "estudie en el colegio aleman, y me gustan los videojuegos, un tiempo fui jugador profesional de juegos de movil y ahora me meti a mi carrera para buscar un futuro mas estable" << endl;
+	system("pause");
+	return 0;
+}
